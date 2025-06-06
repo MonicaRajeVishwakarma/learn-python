9d6b41d7ff65f20727e4356f4bdef9696d4ec855
MonicaRajeVishwakarma <43462756+MonicaRajeVishwakarma@users.noreply.github.com>
2025-06-06 17:38:07 +0200

    Variables usage

---
+++ variables.py
@@ -0,0 +1,8 @@
+character_name_1 = "Tom"
+character_name_2 = "Jerry"
+character_age = "35"
+
+print("Hello, I am character 1 and my name is ", character_name_1 + ".")
+print("Hello, I am character 2 and my name is " + character_name_2 + ".")
+print("I am ", character_age + " years old")
+
