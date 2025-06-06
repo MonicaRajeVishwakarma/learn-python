21d90b5652be1d995251b832747db53d4a9dba40
MonicaRajeVishwakarma <43462756+MonicaRajeVishwakarma@users.noreply.github.com>
2025-06-06 17:55:50 +0200

    Variables usage int and bool

--- variables.py
+++ variables.py
@@ -1,8 +1,14 @@
+from operator import truediv
+
 character_name_1 = "Tom"
 character_name_2 = "Jerry"
-character_age = "35"
+character_age = 35
 
 print("Hello, I am character 1 and my name is ", character_name_1 + ".")
 print("Hello, I am character 2 and my name is " + character_name_2 + ".")
-print("I am ", character_age + " years old")
+print("I am this many years old", + character_age)
+
+is_male = True
+
+print("Is my gender male? ",is_male)
 
